88b44226bf43a114b95e9f62a4f1eb0e9d02e334
aarjuraj <aarjurajarya@gmail.com>
2020-12-17 20:57:30 +0530

    All project Files Added

---
+++ Eventmanager/Calendar/urls.py
@@ -0,0 +1,29 @@
+"""Eventmanager URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from . import views
+
+app_name = 'Calendar'
+urlpatterns = [
+    path('index', views.index, name='index'),
+    path('', views.CalendarView.as_view(), name='calendar'),
+    path('event/new/', views.create_event, name='event_new'),
+    path('event/edit/<int:pk>/', views.EventEdit.as_view(), name='event_edit'),
+    path('event/<int:event_id>/details/', views.event_details, name='event-detail'),
+    path('add_eventmember/<int:event_id>', views.add_eventmember, name='add_eventmember'),
+    path('event/<int:pk>/remove', views.EventMemberDeleteView.as_view(), name="remove_event"),
+]
